dd67937b03733032ea0f37dd84ca664d3ef3e25c
Muia Muteti <fstsmuia@gmail.com>
2021-08-19 14:02:30 +0300

    hero section

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// navigation bar functionality
+
+// reference navbar elements
+let menu = document.querySelector('.menu');
+let menuIcon = document.querySelector('.menu-icon');
+let nav = document.querySelector('.nav');
+
+// icon paths
+const showMenuIcon = "./images/menu.svg";
+const closeMenuIcon = "./images/cross.svg";
+
+// hide or show menu
+// hidden by default
+let showMenu = false;
+
+menu.addEventListener('click', handleMenuClick);
+
+function handleMenuClick() {
+    showMenu = !showMenu;
+    menuIcon.src = showMenu? closeMenuIcon : showMenuIcon;
+    
+    if (showMenu) {
+        nav.classList.add('menu-open');
+    } else {
+        nav.classList.remove('menu-open');
+    }
+}
+
+// end navigation bar functionality
